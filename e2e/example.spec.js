21780e6f9b27a4d8b1d3b2d60c49ad7feda2ceea
Bassem Meghiche <bmeghiche06@gmail.com>
2026-09-23 11:19:44 +0200

    refactor: utilisation de boucle for pour les ue

--- e2e/example.spec.js
+++ e2e/example.spec.js
@@ -36,11 +36,9 @@ test('Should submit the form and make sure it has been submitted', async ({ page
   await page.goto('http://localhost:3000');
   await page.locator('#lastname').fill('Meghiche');
   await page.locator('#firstname').fill('Bassem');
-  await page.locator('#ue1').fill('16');
-  await page.locator('#ue2').fill('19');
-  await page.locator('#ue3').fill('12');
-  await page.locator('#ue4').fill('14');
-  await page.locator('#ue5').fill('18.5');
+  for (let index = 1; index < 6; index++) {
+    await page.locator(`#ue${index}`).fill('16');
+  }
   await page.locator('#parcours').selectOption('Parcours développement web et dispositifs interactifs');
   await page.locator('#portfolio').fill('https://b4ssem.com/');
   await page.locator('button').filter({ hasText: 'Envoyer' }).click();
