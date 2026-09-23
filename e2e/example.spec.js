4bc6736431b2b29588eef2f1cb492208028cb2c9
Bassem Meghiche <bmeghiche06@gmail.com>
2026-09-23 10:52:52 +0200

    partie 3-4

--- e2e/example.spec.js
+++ e2e/example.spec.js
@@ -23,3 +23,26 @@ test('Should visit one news on university\'s website', async ({ page }) => {
   await page.locator('.actusAutres__item a').first().click();
   await expect(page).toHaveURL(/actualites/);
 });
+
+test('Should search for Algeria on Wikipedia and make sure the page is displayed', async ({ page }) => {
+  await page.goto('https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Accueil_principal');
+  await page.getByRole('searchbox', { name: 'Rechercher sur Wikipédia' }).fill('Algérie');
+  await page.getByRole('searchbox', { name: 'Rechercher sur Wikipédia' }).press('Enter');
+  await expect(page).toHaveURL(/Alg%C3%A9rie/);
+});
+
+
+test('Should submit the form and make sure it has been submitted', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+  await page.locator('#lastname').fill('Meghiche');
+  await page.locator('#firstname').fill('Bassem');
+  await page.locator('#ue1').fill('16');
+  await page.locator('#ue2').fill('19');
+  await page.locator('#ue3').fill('12');
+  await page.locator('#ue4').fill('14');
+  await page.locator('#ue5').fill('18.5');
+  await page.locator('#parcours').selectOption('Parcours développement web et dispositifs interactifs');
+  await page.locator('#portfolio').fill('https://b4ssem.com/');
+  await page.locator('button').filter({ hasText: 'Envoyer' }).click();
+  await expect(page.locator('section').nth(0)).toBeVisible();
+});
